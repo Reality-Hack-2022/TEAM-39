7f715e8659ee9e52b8836cb13fd56a5f520030bf
Fanxuan <45900781+fzdesign@users.noreply.github.com>
2022-03-25 21:00:42 -0700

    Initial Unity Project push
    Initial push

---
+++ Project39/Team39MRTK/Assets/DyiHandManipulation.cs
@@ -0,0 +1,242 @@
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using MRTKExtensions.Gesture;
+using UnityEngine;
+using UnityEngine.Events;
+using VRKeys;
+
+namespace DyiPinchGrab
+{
+    public class DyiHandManipulation : MonoBehaviour
+    {
+        /*
+        [SerializeField]
+        private TrackedHandJoint trackedHandJoint = TrackedHandJoint.IndexMiddleJoint;
+
+        [SerializeField]
+        private float grabDistance = 0.1f;
+
+        [SerializeField]
+        private Handedness trackedHand = Handedness.Both;
+        */
+        private Handedness leftHand = Handedness.Left;
+        private Handedness rightHand = Handedness.Right;
+        public Keyboard keyboard;
+
+        int lastLeftFinger = -1;
+        int leftFingerFrameCount = 0;
+
+        int lastRightFinger = -1;
+        int rightFingerFrameCount = 0;
+
+        /*
+        [SerializeField]
+        private bool trackPinch = true;
+
+        [SerializeField]
+        private bool trackGrab = true;
+
+        private IMixedRealityHandJointService handJointService;
+
+        private IMixedRealityHandJointService HandJointService =>
+            handJointService ??
+            (handJointService = CoreServices.GetInputSystemDataProvider<IMixedRealityHandJointService>());
+
+        private MixedRealityPose? previousLeftHandPose;
+
+        private MixedRealityPose? previousRightHandPose;
+        */
+        [SerializeField]
+        public UnityEvent leftIndex;
+        private void Update()
+        {
+            //Debug.Log("Update");
+
+            double[] fingerCurls = new double[8];
+            double threshold = 0.3;
+            double maxLeftFingerCurl = -10.0;
+            double maxRightFingerCurl = -10.0;
+
+            int maxLeftFinger = -1;
+            int maxRightFinger = -1;
+
+
+            fingerCurls[0] = HandPoseUtils.IndexFingerCurl(leftHand);
+            fingerCurls[1] = HandPoseUtils.MiddleFingerCurl(leftHand);
+            fingerCurls[2] = HandPoseUtils.RingFingerCurl(leftHand);
+            fingerCurls[3] = HandPoseUtils.PinkyFingerCurl(leftHand);
+
+            fingerCurls[4] = HandPoseUtils.IndexFingerCurl(rightHand);
+            fingerCurls[5] = HandPoseUtils.MiddleFingerCurl(rightHand);
+            fingerCurls[6] = HandPoseUtils.RingFingerCurl(rightHand);
+            fingerCurls[7] = HandPoseUtils.PinkyFingerCurl(rightHand);
+
+
+            int currentFrame = Time.frameCount;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (fingerCurls[i] >= threshold && fingerCurls[i] > maxLeftFingerCurl)
+                {
+                    // left finger pinch detected
+                    maxLeftFinger = i;
+                    maxLeftFingerCurl = fingerCurls[i];
+
+ 
+                }
+            }
+            for (int i = 4; i < 8; i++)
+            {
+                if (fingerCurls[i] >= threshold && fingerCurls[i] > maxRightFingerCurl)
+                {
+                    // right finger pinch detected
+                    maxRightFinger = i;
+                    maxRightFingerCurl = fingerCurls[i];
+
+
+                }
+            }
+
+            // update frame count on each hand if same pinch as last frame, otherwise update and reset frame counts
+            if (lastLeftFinger == maxLeftFinger)
+            {
+                leftFingerFrameCount++;
+            }
+            else
+            {
+                lastLeftFinger = maxLeftFinger;
+                leftFingerFrameCount = 0;
+            }
+
+            if (lastRightFinger == maxRightFinger)
+            {
+                rightFingerFrameCount++;
+            }
+            else
+            {
+                lastRightFinger = maxRightFinger;
+                rightFingerFrameCount = 0;
+            }
+
+
+
+            // if left hand pinching and 15 frames have passed since pinching, then indicate
+            if (maxLeftFinger >= 0)
+            {
+                if (leftFingerFrameCount >= 15)
+                {
+                    // moves up column
+                    if (maxLeftFinger >= 4) keyboard.IndicatePress(maxLeftFinger);
+                }
+            } else
+            {
+                if (lastLeftFinger == -1)  {
+                    leftFingerFrameCount++;
+
+                    if (leftFingerFrameCount >= 15)
+                    {
+                        Debug.Log("no curl");
+                    }
+                } else
+                {
+                    lastLeftFinger = -1;
+                    leftFingerFrameCount++;
+                }
+            }
+
+            if (maxRightFinger >= 0)
+            {
+                if (rightFingerFrameCount >= 15)
+                {
+                    // if column indicated, indicate row
+                    if (maxRightFinger >= 4) keyboard.IndicateRowPress(maxRightFinger);
+                }
+            }
+            else
+            {
+                if (lastRightFinger == -1)
+                {
+                    rightFingerFrameCount++;
+
+                    if (rightFingerFrameCount >= 15)
+                    {
+                        Debug.Log("no curl");
+                    }
+                }
+                else
+                {
+                    lastRightFinger = -1;
+                    rightFingerFrameCount++;
+                }
+            }
+
+
+
+            /*
+            var leftHandPose = GetHandPose(Handedness.Left, previousLeftHandPose != null);
+            var rightHandPose = GetHandPose(Handedness.Right, previousRightHandPose != null);
+            {
+                var jointTransform = HandJointService.RequestJointTransform(trackedHandJoint, trackedHand);
+                if (rightHandPose != null && previousRightHandPose != null)
+                {
+                    if (leftHandPose != null && previousLeftHandPose != null)
+                    {
+                        // fight! pick the closest one
+                        var isRightCloser = Vector3.Distance(rightHandPose.Value.Position, jointTransform.position) <
+                                            Vector3.Distance(leftHandPose.Value.Position, jointTransform.position);
+
+                        ProcessPoseChange(
+                            isRightCloser ? previousRightHandPose : previousLeftHandPose,
+                            isRightCloser ? rightHandPose : leftHandPose);
+                    }
+                    else
+                    {
+                        ProcessPoseChange(previousRightHandPose, rightHandPose);
+                    }
+                }
+                else if (leftHandPose != null && previousLeftHandPose != null)
+                {
+                    ProcessPoseChange(previousLeftHandPose, leftHandPose);
+                }
+            }
+            previousLeftHandPose = leftHandPose;
+            previousRightHandPose = rightHandPose;
+            */
+        }
+        /*
+        private MixedRealityPose? GetHandPose(Handedness hand, bool hasBeenGrabbed)
+        {
+            if ((trackedHand & hand) == hand)
+            {
+                if (HandJointService.IsHandTracked(hand) &&
+                    ((GestureUtils.IsPinching(hand) && trackPinch) ||
+                     (GestureUtils.IsGrabbing(hand) && trackGrab)))
+                {
+                    var jointTransform = HandJointService.RequestJointTransform(trackedHandJoint, hand);
+                    var palmTransForm = HandJointService.RequestJointTransform(TrackedHandJoint.Palm, hand);
+                    Debug.Log("Index " + HandPoseUtils.RingFingerCurl(leftHand));
+
+                    if (hasBeenGrabbed ||
+                       Vector3.Distance(gameObject.transform.position, jointTransform.position) <= grabDistance)
+                    {
+
+                        return new MixedRealityPose(jointTransform.position, palmTransForm.rotation);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private void ProcessPoseChange(MixedRealityPose? previousPose, MixedRealityPose? currentPose)
+        {
+            var delta = currentPose.Value.Position - previousPose.Value.Position;
+            var deltaRotation = Quaternion.FromToRotation(previousPose.Value.Forward, currentPose.Value.Forward);
+            gameObject.transform.position += delta;
+            gameObject.transform.rotation *= deltaRotation;
+        }
+
+        */
+    }
+}
---
+++ Project39/Team39MRTK/Assets/VRKeys/Scripts/Key.cs
@@ -0,0 +1,336 @@
+﻿/**
+ * Copyright (c) 2017 The Campfire Union Inc - All Rights Reserved.
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for
+ * full license information.
+ *
+ * Email:   info@campfireunion.com
+ * Website: https://www.campfireunion.com
+ */
+
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+namespace VRKeys {
+
+	/// <summary>
+	/// An individual key in the VR keyboard.
+	/// </summary>
+	public class Key : MonoBehaviour {
+		public Keyboard keyboard;
+
+		public TextMeshPro label;
+
+		public Material inactiveMat;
+
+		public Material activeMat;
+
+		public Material disabledMat;
+
+		public Vector3 defaultPosition;
+
+		public Vector3 indicatePosition;
+		public Vector3 indicatePressPosition;
+
+
+		public Vector3 pressedPosition;
+
+		public Vector3 pressDirection = Vector3.down;
+		public Vector3 indicateDirection = Vector3.up;
+
+		public float pressMagnitude = 0.1f;
+
+		public bool autoInit = false;
+
+		private bool isPressing = false;
+		private bool isIndicating = false;
+
+		private bool disabled = false;
+
+		protected MeshRenderer meshRenderer;
+
+		private IEnumerator _ActivateFor;
+
+		private IEnumerator _Press;
+		private IEnumerator _Indicate;
+
+		public KeyGroup keyGroup;
+		public int keyRow;
+
+		private void Awake () {
+			meshRenderer = GetComponent<MeshRenderer> ();
+	
+			if (autoInit) {
+				Init (transform.localPosition);
+			}
+		}
+		private KeyGroup getKeyGroup() {
+			if (label.text == "q" || label.text == "a" || label.text == "z") {
+				return KeyGroup.YellowLeft;
+			} else if (label.text == "w" || label.text == "s" || label.text == "x") {
+				return KeyGroup.YellowRight;
+			} else if (label.text == "e" || label.text == "d" || label.text == "c") {
+				return KeyGroup.RedLeft;
+			} else if (label.text == "r" || label.text == "f" || label.text == "v") {
+				return KeyGroup.RedRight;
+			} else if (label.text == "t" || label.text == "g" || label.text == "b") {
+				return KeyGroup.GreenLeft;
+			} else if (label.text == "y" || label.text == "h" || label.text == "n") {
+				return KeyGroup.GreenMiddle;
+			} else if (label.text == "u" || label.text == "j" || label.text == "m") {
+				return KeyGroup.GreenRight;
+			} else if (label.text == "i" || label.text == "k") {
+				return KeyGroup.BlueLeft;
+			} else if (label.text == "o" || label.text == "l" || label.text == "p") {
+				return KeyGroup.BlueRight;
+			}
+			return KeyGroup.Default;
+		}
+
+		public int getKeyRow() {
+			if ("qwertyuio".Contains(label.text)) {
+				return 0;
+			} else if ("asdfghjkl".Contains(label.text)) {
+				return 1;
+			} else if ("zxcvbnmp".Contains(label.text)) {
+				return 2;
+			}
+			return -1;
+		}
+
+		public void initGroup() {
+			this.keyGroup = getKeyGroup();
+			this.keyRow = getKeyRow();
+
+			switch (keyGroup) {
+				case KeyGroup.YellowLeft:
+					meshRenderer.material.SetColor("_Color", Color.yellow);
+					break;
+				case KeyGroup.YellowRight:
+					meshRenderer.material.SetColor("_Color", Color.yellow);
+					break;
+				case KeyGroup.RedLeft:
+					meshRenderer.material.SetColor("_Color", Color.red);
+					break;
+				case KeyGroup.RedRight:
+					meshRenderer.material.SetColor("_Color", Color.red);
+					break;
+				case KeyGroup.GreenLeft:
+					meshRenderer.material.SetColor("_Color", Color.green);
+					break;
+				case KeyGroup.GreenMiddle:
+					meshRenderer.material.SetColor("_Color", Color.green);
+					break;
+				case KeyGroup.GreenRight:
+					meshRenderer.material.SetColor("_Color", Color.green);
+					break;
+				case KeyGroup.BlueLeft:
+					meshRenderer.material.SetColor("_Color", Color.blue);
+					break;
+				case KeyGroup.BlueRight:
+					meshRenderer.material.SetColor("_Color", Color.blue);
+					break;
+				case KeyGroup.Default:
+					meshRenderer.material.SetColor("_Color", Color.gray);
+					break;
+				default:
+					break;
+			}
+		}
+
+		/// <summary>
+		/// Initialize the key with a default position and pressed position.
+		/// </summary>
+		/// <param name="defaultPos">Default position.</param>
+		public void Init (Vector3 defaultPos) {
+			defaultPosition = defaultPos;
+			pressedPosition = defaultPos + (Vector3.down * 0.01f);
+			indicatePosition = defaultPos + (Vector3.up * 0.1f);
+			indicatePressPosition = indicatePosition + (Vector3.up * 0.1f);
+		}
+
+		private void OnEnable () {
+			isPressing = false;
+			disabled = false;
+			transform.localPosition = defaultPosition;
+			meshRenderer.material = inactiveMat;
+
+			OnEnableExtras ();
+		}
+
+		/// <summary>
+		/// Override this to add custom logic on enable.
+		/// </summary>
+		protected virtual void OnEnableExtras () {
+			//meshRenderer.material.SetColor("_Color", Color.red);
+			initGroup();
+		}
+
+		public void OnTriggerEnter (Collider other) {
+			if (isPressing || disabled || keyboard.disabled || !keyboard.initialized) {
+				return;
+			}
+
+			Mallet mallet = other.gameObject.GetComponent<Mallet> ();
+			if (mallet != null) {
+				if (!mallet.isMovingDownward) {
+					return;
+				}
+
+				if (mallet.hand == Mallet.MalletHand.Left && keyboard.leftPressing) {
+					return;
+				} else if (mallet.hand == Mallet.MalletHand.Right && keyboard.rightPressing) {
+					return;
+				}
+
+				if (_Press != null && _Press.MoveNext ()) {
+					StopCoroutine (_Press);
+				}
+				_Press = Press (other, mallet);
+				StartCoroutine (_Press);
+			}
+		}
+
+		private IEnumerator Press (Collider other, Mallet mallet) {
+			isPressing = true;
+
+			/*
+			if (mallet.hand == Mallet.MalletHand.Left) {
+				keyboard.leftPressing = true;
+			} else if (mallet.hand == Mallet.MalletHand.Right) {
+				keyboard.rightPressing = true;
+			}
+			*/
+
+			//mallet.HandleTriggerEnter (this);
+			HandleTriggerEnter (other);
+
+			transform.localPosition = pressedPosition;
+
+			yield return new WaitForSeconds (0.125f);
+
+			transform.localPosition = defaultPosition;
+			isPressing = false;
+
+			/*
+			if (mallet.hand == Mallet.MalletHand.Left) {
+				keyboard.leftPressing = false;
+			} else if (mallet.hand == Mallet.MalletHand.Right) {
+				keyboard.rightPressing = false;
+			}
+			*/
+		}
+
+		/// <summary>
+		/// Override this to handle trigger events. Only fires when
+		/// a downward trigger event occurred from the collider
+		/// matching keyboard.colliderName.
+		/// </summary>
+		/// <param name="other">Collider.</param>
+		public virtual void HandleTriggerEnter (Collider other) {
+			// Override me!
+		}
+
+		/// <summary>
+		/// Show the active material for the specified length of time.
+		/// </summary>
+		/// <param name="seconds">Seconds.</param>
+		public void ActivateFor (float seconds) {
+			if (_ActivateFor != null && _ActivateFor.MoveNext ()) {
+				StopCoroutine (_ActivateFor);
+			}
+			_ActivateFor = DoActivateFor (seconds);
+			StartCoroutine (_ActivateFor);
+		}
+
+		private IEnumerator DoActivateFor (float seconds) {
+			meshRenderer.material = activeMat;
+			yield return new WaitForSeconds (seconds);
+
+			meshRenderer.material = inactiveMat;
+		}
+
+		/// <summary>
+		/// Disable the key.
+		/// </summary>
+		public virtual void Disable () {
+			disabled = true;
+
+			if (meshRenderer != null) {
+				meshRenderer.material = disabledMat;
+			}
+		}
+
+		/// <summary>
+		/// Re-enable a disabled key.
+		/// </summary>
+		public virtual void Enable () {
+			disabled = false;
+
+			if (meshRenderer != null) {
+				meshRenderer.material = inactiveMat;
+			}
+		}
+
+		/// <summary>
+		/// Update the key's label from a new language.
+		/// </summary>
+		/// <param name="translation">Translation object.</param>
+		public virtual void UpdateLayout (Layout translation) {
+			// Override me!
+		}
+
+
+
+		public void updatePosition() {
+			if (isIndicating && isPressing) {
+				transform.localPosition = indicatePressPosition;
+			} else if (isIndicating) {
+				transform.localPosition = indicatePosition;
+			} else {
+				transform.localPosition = defaultPosition;
+			}
+		}
+
+		public void Indicate(bool indicate) {
+			if (_Indicate != null && _Indicate.MoveNext ()) {
+				StopCoroutine (_Indicate);
+			}
+			_Indicate = IndicateThread(indicate);
+			StartCoroutine (_Indicate);
+		}
+
+		public IEnumerator IndicateThread(bool indicate) {
+			if (indicate) {
+				isIndicating = true;
+				updatePosition();
+			} else {
+				yield return new WaitForSeconds (2f);
+				isIndicating = false;
+				updatePosition();
+				keyboard.ResetKeyGroup();
+			}
+		}
+
+		public void IndicatePress(bool press) {
+			if (_Press != null && _Press.MoveNext ()) {
+				StopCoroutine (_Press);
+			}
+			_Press = IndicateThreadPress(press);
+			StartCoroutine (_Press);
+		}
+
+		public IEnumerator IndicateThreadPress(bool press) {
+			if (press) {
+				isPressing = true;
+				updatePosition();
+			} else {
+				yield return new WaitForSeconds (2f);
+				isPressing = false;
+				updatePosition();
+				keyboard.ResetRow();
+			}
+		}
+	}
+}
